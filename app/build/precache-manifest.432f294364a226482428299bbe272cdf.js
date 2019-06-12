890057f8b0c4142e77750b494cc96166b918f7e6
林晓生 <798738148@qq.com>
2019-06-12 15:01:10 +0800

    添加构建文件

---
+++ app/build/precache-manifest.432f294364a226482428299bbe272cdf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22363b4e893b274aa858b9076943eb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a02f8a2b41d6b0999862",
+    "url": "/static/css/2.d4061aad.chunk.css"
+  },
+  {
+    "revision": "baf850cafbd3c1fdb8da",
+    "url": "/static/css/main.1df31330.chunk.css"
+  },
+  {
+    "revision": "a02f8a2b41d6b0999862",
+    "url": "/static/js/2.63d2563d.chunk.js"
+  },
+  {
+    "revision": "baf850cafbd3c1fdb8da",
+    "url": "/static/js/main.9ffdc202.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ea88ee822c55f196a9295ed92507c533",
+    "url": "/static/media/bloger-head.ea88ee82.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
